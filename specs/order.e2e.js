6e17dc46c1c80a5db557f4af96b2bbf4982a87e2
Zradula <lukas.zradula@sap.com>
2022-12-14 20:54:53 +0100

    filling & submit order

--- specs/order.e2e.js
+++ specs/order.e2e.js
@@ -1,3 +1,4 @@
+import { getFieldValueById } from '../pages/functions.js'
 import {
     ICO,
     clientName,
@@ -14,4 +15,67 @@ describe('Objednávka pro MŠ/ZŠ', () => {
 
     // testy sem :)
 
+    beforeEach(() => {
+        browser.reloadSession();
+        browser.url('/objednavka/pridat');
+    });
+
+    it('Vyplnenie objednavky', () => {
+        
+        const card = $('.card');
+        expect(card).toBeDisplayed();
+
+        const ico = $('#ico');
+        expect(ico).toBeEnabled();
+
+        ico.setValue(ICO);
+        browser.keys('Enter');
+        browser.pause(5000);
+
+        // expect(odberatel.getText()).toEqual(clientName);
+        const odberatel = getFieldValueById('client');
+        console.log('Odberatel je: ' + odberatel);
+        expect(odberatel).toEqual(clientName);
+
+        const adresa = getFieldValueById('address');
+        console.log('Uplna adresa je: ' + address);
+
+        const zastoupena = $('#substitute');
+        zastoupena.setValue(substituteName);
+
+        const kontaknaOsoba = $('#contact_name');
+        kontaknaOsoba.setValue(contactName);
+
+        const telefon = $('#contact_tel');
+        telefon.setValue(contactPhone);
+
+        const email = $('#contact_mail');
+        email.setValue(contactEmail);
+
+        const zaciatok = $('#start_date_1');
+        zaciatok.setValue(startDate);
+
+        const koniec = $('#end_date_1');
+        koniec.setValue(endDate);
+
+        const primestkyTabor = $('#nav-home-tab');
+        primestkyTabor.click();
+
+        const kurz = $('#camp-date_part');
+        kurz.selectByIndex(0);
+
+        const pocetDeti = $('#camp-students');
+        pocetDeti.setValue('3');
+
+        const vek = $('#camp-age');
+        vek.setValue('18');
+
+        const pocetPedagDoprovodu = $('#camp-adults');
+        pocetPedagDoprovodu.setValue(2);
+
+        const ulozit = $('.btn-primary');
+        ulozit.click();
+
+    });
+    
 });
