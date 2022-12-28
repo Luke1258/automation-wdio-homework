14d9990662b1e061b117404231ab0db1936ac693
Zradula <lukas.zradula@sap.com>
2022-12-28 19:39:38 +0100

    adding allure-report

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,3 +1,20 @@
+// const fs = require('fs');
+// const passedDirectory = 'screenshots/passed';
+// const failedDirectory = 'screenshots/failed';
+
+// function createIfNotExists(dir) {
+//     if (!fs.existsSync(dir)) {
+//         fs.mkdirSync(dir, { recursive: true });
+//     }
+// }
+
+// function deleteFiles(dir) {
+//     fs.rmdir(dir, { recursive: true }, err => {
+//         if (err) console.log(err);
+//     });
+// }
+const allure = require('allure-commandline');
+
 exports.config = {
     specs: [
         './specs/**/*.js'
@@ -37,9 +54,48 @@ exports.config = {
         'selenium-standalone'
     ],
     framework: 'mocha',
-    reporters: ['spec'],
+    reporters: [
+        'spec',
+        ['allure', {
+        outputDir: 'allure-results',
+        disableWebdriverStepsReporting: true,
+        disableWebdriverScreenshotsReporting: false,
+    }]],
     mochaOpts: {
         ui: 'bdd',
         timeout: 60000
+    },
+    onComplete: function() {
+        const reportError = new Error('Could not generate Allure report')
+        const generation = allure(['generate', 'allure-results', '--clean'])
+        return new Promise((resolve, reject) => {
+            const generationTimeout = setTimeout(
+                () => reject(reportError),
+                5000)
+
+            generation.on('exit', function(exitCode) {
+                clearTimeout(generationTimeout)
+
+                if (exitCode !== 0) {
+                    return reject(reportError)
+                }
+
+                console.log('Allure report successfully generated')
+                resolve()
+            })
+        })
     }
+    // onPrepare: (config, capabilities) => {
+    //     deleteFiles("screenshots");
+    // },
+    // afterTest: (test, context, { error, result, duration, passed, retries }) => {
+    //     const screenshotName = (`${test.parent}__${test.title}.png`).replace(/ /g, '_');
+    //     if (passed === true) {
+    //         createIfNotExists(passedDirectory);
+    //         browser.saveScreenshot( `${passedDirectory}/failed/${screenshotName}`);
+    //     } else {
+    //         createIfNotExists(failedDirectory);
+    //         browser.saveScreenshot(`${failedDirectory}/failed/${screenshotName}`);
+    //     }
+    // }
 }
